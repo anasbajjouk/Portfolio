39347cdcde81b03400468e2fcc063736a0adf348
Anas Bajjouk <bajjouk1@gmail.com>
2019-12-24 21:00:21 +0100

    Updates

---
+++ precache-manifest.b2c62b88bdf7a1a5642f069ea6e6021f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2811dce503b6a05022efcae2c189620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "289080b836086eb91bc1",
+    "url": "/static/css/main.e954e8c9.chunk.css"
+  },
+  {
+    "revision": "0f0aa813a4517b44894f",
+    "url": "/static/js/2.a342bf07.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.a342bf07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "289080b836086eb91bc1",
+    "url": "/static/js/main.cb991c3f.chunk.js"
+  },
+  {
+    "revision": "56949dc0caeae51e0583",
+    "url": "/static/js/runtime-main.cb5ad4cb.js"
+  },
+  {
+    "revision": "1112695395b75294c44cf540c9537e3a",
+    "url": "/static/media/Quantum.11126953.otf"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/static/media/html.24845c5c.svg"
+  },
+  {
+    "revision": "a745037d8380b653a8dbb6a16d4c449c",
+    "url": "/static/media/javascript.a745037d.svg"
+  },
+  {
+    "revision": "4337687002ea919ee3f4dc6c8853c576",
+    "url": "/static/media/nodejs-icon.43376870.svg"
+  },
+  {
+    "revision": "21bba51ed14e93f325cd0d22712bcb18",
+    "url": "/static/media/quantum-webfont.21bba51e.woff"
+  },
+  {
+    "revision": "645f25dafcafb5c8d497eb7cea05e35d",
+    "url": "/static/media/quantum-webfont.645f25da.woff2"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/static/media/react.f0793d9f.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "950c373a95a9431712049cc218a07a4a",
+    "url": "/static/media/sass.950c373a.svg"
+  }
+]);
