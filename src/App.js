dccbd02e15252f49b69bb62b6f0fc9e9db3be563
Anas Bajjouk <bajjouk1@gmail.com>
2019-12-24 19:42:30 +0100

    Porfolio completed - Build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/header/header.component";
+import About from "./components/about/about.component";
+import Projects from "./components/projects/projects.component";
+import ContactUs from "./components/contact-us/contact-us.component";
+import Footer from "./components/footer/footer.component";
+
+//Styles
+import { AppWrapper } from "./App.styles";
 
 function App() {
+  const [name] = useState("Anas Bajjouk");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Header name={name} />
+      <About />
+      <Projects />
+      <ContactUs />
+      <Footer />
+    </AppWrapper>
   );
 }
 
