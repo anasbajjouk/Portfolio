474045531388971ec43c7953e046a707e0cc1bd0
Anas Bajjouk <bajjouk1@gmail.com>
2019-12-26 00:32:37 +0100

    small update

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,8 +13,8 @@ export const PROJECTS = [
     id: 2,
     name: "Tetrominos Game",
     github: "https://github.com/anasbajjouk/react-tetrominos",
-    emo_URL: "",
-    stack: "HTML5 - CSS3 - REACT(Hooks)",
+    demo_URL: "https://anasbajjouk.github.io/react-tetrominos/",
+    stack: "HTML5 - CSS3 - REACT(Hooks, memo) - Styled components",
     bgImage:
       "https://images-platform.99static.com//h4uaMllO6_QpNE9spVSA_eBDqKg=/0x1:598x599/fit-in/500x500/projects-files/29/2973/297372/fc771dcc-cb62-4e22-aee0-7183de1f370c.jpg"
   },
@@ -23,7 +23,7 @@ export const PROJECTS = [
     id: 3,
     name: "Shopping List",
     github: "https://github.com/anasbajjouk/MERN-shopping-list",
-    emo_URL: "https://github.com/anasbajjouk/Bikes",
+    demo_URL: "https://shopping-list-mern-app.herokuapp.com/",
     stack: "MongoDb - Express - React - NodeJs",
     bgImage:
       "https://images-platform.99static.com//jSVo9L3fY0s3BK1SK8trF4kRCgI=/601x0:1199x598/fit-in/500x500/99designs-contests-attachments/105/105491/attachment_105491409"
@@ -33,7 +33,7 @@ export const PROJECTS = [
     id: 4,
     name: "ChatKat",
     github: "https://github.com/anasbajjouk/chat-app-chatkit",
-    emo_URL: "",
+    demo_URL: "https://anasbajjouk.github.io/chat-app-chatkit/",
     stack: "React - Push chatKit API",
     bgImage:
       "https://images-platform.99static.com//h4uaMllO6_QpNE9spVSA_eBDqKg=/0x1:598x599/fit-in/500x500/projects-files/29/2973/297372/fc771dcc-cb62-4e22-aee0-7183de1f370c.jpg"
@@ -43,7 +43,7 @@ export const PROJECTS = [
     id: 5,
     name: "Monster Rolodex",
     github: "https://github.com/anasbajjouk/react-monsters-rolodex",
-    emo_URL: "https://anasbajjouk.github.io/react-monsters-rolodex/",
+    demo_URL: "https://anasbajjouk.github.io/react-monsters-rolodex/",
     stack: "HTML5 - CSS3 - REACT",
     bgImage:
       "https://images-platform.99static.com//jSVo9L3fY0s3BK1SK8trF4kRCgI=/601x0:1199x598/fit-in/500x500/99designs-contests-attachments/105/105491/attachment_105491409"
@@ -53,7 +53,7 @@ export const PROJECTS = [
     id: 6,
     name: "BookEvery",
     github: "https://github.com/anasbajjouk/bookevery-template",
-    emo_URL: "https://anasbajjouk.github.io/bookevery-template/",
+    demo_URL: "https://anasbajjouk.github.io/bookevery-template/",
     stack: "HTML5 - CSS3 (Sass)",
     bgImage:
       "https://images-platform.99static.com//h4uaMllO6_QpNE9spVSA_eBDqKg=/0x1:598x599/fit-in/500x500/projects-files/29/2973/297372/fc771dcc-cb62-4e22-aee0-7183de1f370c.jpg"
@@ -62,9 +62,19 @@ export const PROJECTS = [
     id: 7,
     name: "UPH (Ultimate Personal Homes)",
     github: "https://github.com/anasbajjouk/UPH",
-    emo_URL: "https://anasbajjouk.github.io/UPH/",
+    demo_URL: "https://anasbajjouk.github.io/UPH/",
     stack: "HTML5 - CSS3 (Sass)",
     bgImage:
       "https://images-platform.99static.com//jSVo9L3fY0s3BK1SK8trF4kRCgI=/601x0:1199x598/fit-in/500x500/99designs-contests-attachments/105/105491/attachment_105491409"
+  },
+
+  {
+    id: 8,
+    name: "e-tours",
+    github: "https://github.com/anasbajjouk/e-tours-template",
+    demo_URL: "https://anasbajjouk.github.io/e-tours-template/",
+    stack: "HTML5 - CSS3 (Sass) Float method ",
+    bgImage:
+      "https://images-platform.99static.com//h4uaMllO6_QpNE9spVSA_eBDqKg=/0x1:598x599/fit-in/500x500/projects-files/29/2973/297372/fc771dcc-cb62-4e22-aee0-7183de1f370c.jpg"
   }
 ];
